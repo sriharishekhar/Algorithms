6fe0c55f2bf315368128c617fb8c5c15e179759c
Srihari Shekhar <prime.sriharishekhar@gmail.com>
2019-07-24 18:47:16 +0530

    generic change

--- selection_sort_in_vectors.cpp
+++ selection_sort_in_vectors.cpp
@@ -40,3 +40,47 @@ int main() {
   selection_sort(v);
   return 0;
 }
+
+
+/*
+#include <iostream>
+#include <vector>
+#include <iterator>
+using namespace std;
+
+void swap (vector <char> &v, int min, int j) {
+  int temp;
+  temp = v[min];
+  v[min] = v[j];
+  v[j] = temp; 
+}
+
+void display(vector <char> &v) {
+  for (int i = 0; i < v.size(); i++) {
+    cout << v[i] << " ";
+  }
+}
+
+void selection_sort(vector <char> v) {
+  vector <char> :: iterator itr1;
+  vector <char> :: iterator itr2;
+  vector <char> :: iterator itr3;
+  int min, i, j;
+  for (i = 0; i < v.size(); i++) {
+    min = i;
+    for (j = i; j < v.size(); j++) {
+      if (v[min] > v[j]) {
+        min = j;
+      }      
+    }
+    swap (v,min,j);
+  } 
+  display(v);
+}
+
+int main() {
+  string s("amanjumpedoverabuilding");  
+  vector <char> v (s.begin(), s.end());
+  selection_sort(v);
+  return 0;
+}/*
