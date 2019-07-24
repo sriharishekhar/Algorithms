f842a9683b37caddea65175056f8eac7e1e3c19f
Srihari Shekhar <52935613+sriharishekhar@users.noreply.github.com>
2019-07-24 05:50:03 +0530

    Update insertion_sort_vector.cpp

--- insertion_sort_vector.cpp
+++ insertion_sort_vector.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void print(vector <int> &v) {
   vector <int> :: iterator itr;
   for (itr = v.begin(); itr < v.end(); itr++) {
-  	cout << *itr << " ";
+    cout << *itr << " ";
   }
 }
 
@@ -24,6 +24,7 @@ void insertion_sort(vector <int> v) {
 }
 
 int main() {
+
 int a[] = {9,8,7,1,4,3,3,1,7,4};
 int n = sizeof(a)/sizeof(a[0]);  
 vector <int> v(a, a + n);
