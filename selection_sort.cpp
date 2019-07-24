ea6266a404efbdcb38df8500c431dc11f787b2f0
Srihari Shekhar <52935613+sriharishekhar@users.noreply.github.com>
2019-07-24 15:55:11 +0530

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,52 +1,42 @@
+// Q. Implement Selection Sort
+// Algorithm -
+// Traverse from 0 to n, Find smallest element and place it at index 0
+// Now traverse from 1 to n, Find smallest element and place it at index 1
+// Repeat till n
+
 #include <iostream>
 using namespace std;
 
-void swap(int *a, int *b)
-{
-  int temp;  
+void swap (int *a, int *b) {
+  int temp;
   temp = *a;
   *a = *b;
   *b = temp;
 }
 
-void selection_sort(int a[], int n)
-{
-	int i, j, min;  
-
-	for (int i = 0; i < n; i++)
-	{
-	    for (i = 0; i < n; i++)  
-    	{  
-        	min = i;  
-        	for (j = i+1; j < n; j++)  
-        	if (a[j] < a[min])  
-            min = j;  
-  
-  	        swap(&a[min], &a[i]); 		
-		}
-
-		cout << "Sorted array = ";
-	
-		for (int i = 0; i < n; i++)
-		{ 
-			cout << a[i] << " ";
-		}
-	}
+void print (int a[], int n) {
+  for (int i = 0; i < n; i++) {
+  	cout << a[i] << " ";
+  }
 }
 
-int main()
-{
-	int a[] = {5,21,3,14,1};
-	int n = sizeof(a)/sizeof(a[0]);
-	
-	cout << "Unsorted array = ";
-	
-	for (int i = 0; i < n; i++)
-	{ 
-		cout << a[i] << " ";
-	}
-	cout << endl;
-	
-	selection_sort(a, n);
-	return 0;
+void selection_sort(int a[], int n) {
+  int min_index;
+  for (int i = 0; i < n; i++) {
+    min_index = i;
+    for (int j = i; j < n; j++) {
+      if (a[min_index] > a[j]) 
+        min_index = j;
+      }
+    swap (&a[min_index], &a[i]);
+  }
+  print(a, n);
+  cout << endl;
 }
+
+int main() {
+  int a[] = {8,9,12,5,0,4};
+  int n = sizeof(a)/sizeof(a[0]);
+  selection_sort(a,n);
+  return 0;
+} 
