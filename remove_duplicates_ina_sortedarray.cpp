08d290aa5b022ea97a105fcf60a7a31aaa926d5d
Srihari Shekhar <52935613+sriharishekhar@users.noreply.github.com>
2019-08-10 12:17:14 -0700

    Update remove_duplicates_ina_sortedarray.cpp

--- remove_duplicates_ina_sortedarray.cpp
+++ remove_duplicates_ina_sortedarray.cpp
@@ -1,31 +1,27 @@
 //Q. Remove duplicates in a sorted array
-// Algo : 
+// Algo : Array is sorted, so we copy element when a new element appears
+// run a foor loop i 
+// if s[i] is not equal to s[index] where index is initialized to 0, copy s[i] over s[index + 1]
+// next, when s[i] != s[index], move new s[i] to next position of s[index]
+// do this for every value of i 
+// the string now(without duplicates) should contain index number of elements i.e new size of string = index + 1
 
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-void remove_duplicate_ina_sortedarray(string& s) {
+void remove_duplicate(string& s) {
+  int index = 0;
   for (int i = 0; i < s.size(); i++) {
-    int index = i + 1;
-    for (int j = i; j < s.size(); j++) {
-      if (s[i] != s[j]) {
-        s[index] = s[j];
-        index++;
-      }
-      if (i == s.size() - 1) {
-        int k = i;
-        while (s[k] == s[k-1]) {
-          s.pop_back();
-          k--;
-        }
-      }
+    if (s[i] != s[index]) {
+      s[++index] = s[i];
     }
   }
+  s.resize(index+1);
 }
 
-void print(string s) {
+void print(const string& s) {
   cout << "String after duplicates are removed: ";
   for (int i = 0; i < s.size(); i++) {
     cout << s[i];
@@ -35,9 +31,9 @@ void print(string s) {
 
 int main() {
   
-  string s = "aabbbcccddefgghhhijjklmssuuswxyyzz";
+  string s = "aabbbcccddefgghhhijjklmssuuswxyyzzzz";
   cout << "String before duplicates are removed: " << s << endl;
-  remove_duplicate_ina_sortedarray(s);
+  remove_duplicate(s);
   print(s);
 
   return 0;
