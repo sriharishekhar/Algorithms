24ca2b5bf41ff59080b7843bbd4294801a99d315
Srihari Shekhar <52935613+sriharishekhar@users.noreply.github.com>
2019-08-11 09:36:05 -0700

    Update sort_bubble.cpp

--- sort_bubble.cpp
+++ sort_bubble.cpp
@@ -1,14 +1,3 @@
-// Q. Optimized bubble sort
-// Algo:
-// Traverse the array and compare every next pair of elements
-// if a[i] > a[i+1], swap a[i] & a[i+1]
-// at the end of first traversal, the biggest element is at the last index of the array and hence is sorted
-// needs at most n more traversals to sort every element and next traversals need only be till n - i
-// for optimized bubble sort, we need to stop the loop as soon as no swap is done after one whole traversal
-// initialize bool variable as false at the beginning of first for loop
-// if swap is done, bool = true
-// if bool == false after one complete traversal of inner loop, break.
-
 #include <iostream>
 
 using namespace std;
@@ -43,7 +32,7 @@ void optimized_bubble_sort(int a[], int n) {
 }
 
 int main() {
-  int a[] = {13, 4, 1, 42, 3, 6};
+   {13, 4, 1, 42, 3, 6};
   int n = sizeof(a)/sizeof(a[0]);
   cout << "Unsorted array: ";
   print(a,n);
