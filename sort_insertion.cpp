dea9cf3d921e8b8424a5f1dde04f9bddf6b9ce37
Srihari Shekhar <52935613+sriharishekhar@users.noreply.github.com>
2019-08-11 12:42:10 -0700

    Update sort_insertion.cpp

--- sort_insertion.cpp
+++ sort_insertion.cpp
@@ -1,41 +1,46 @@
 // Insertion sort
 // Algo: At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list, and inserts it there. 
 // Repeat until no input elements remain. 
-// At each array-position, check the value there against the largest value in the sorted list 
+// At each vector-position, check the value there against the largest value in the sorted list 
 // If larger, leave the element in place and move to the next
 // If smaller, find the correct position within the sorted list, shift all the larger values up to make a space, and insert into that correct position.
 
 // Time Complexity : O(n^2)
 
 // Uses : 1. Insertion sort is used when number of elements is small. 
-//        2. It can also be useful when input array is almost sorted, only few elements are misplaced in complete big array.
+//        2. It can also be useful when input vector is almost sorted, only few elements are misplaced in complete big vector.
 
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-void print(int a[], int n) {
-  for (int i = 0; i < n; i++) {
-    cout << a[i] << " ";
+void insertionsort(vector<int>& v) {
+  for (int i = 1; i < v.size(); i++) {
+  	int j, key;
+    j = i - 1;
+    key = v[i];  
+  	while (j >= 0 && v[j] > key) {
+      v[j+1] = v[j];
+      j--;
+    }
+    v[j+1] = key;
   }
 }
 
-void insertion_sort(int a[], int n) {
-  int i, j, key;
-  for (i = 0; i <n; i++) {
-    key = a[i];
-    j = i - 1;
-    while (j >= 0 && a[j]>key) {
-      a[j+1] = a[j];
-      j--;
-    }
-    a[j+1] = key;
-  }  
-  print(a,n);
+void print(const vector<int>& v) {
+  for (int i = 0; i < v.size(); i++) {
+  	cout << v[i] << " ";
+  }
+  cout << endl;
 }
 
 int main() {
-  int a[] = {9, 8, 7, 1, 4, 3, 3, 1, 7 ,4};
-  int n = sizeof(a)/sizeof(a[0]);
-  insertion_sort(a,n);
+  vector<int> v {13,4,1,42,3,6};
+  cout << "Unsorted Vector: ";
+  print(v);
+  insertionsort(v);
+  cout << "Sorted Vector: ";
+  print(v);
   return 0;
 }
