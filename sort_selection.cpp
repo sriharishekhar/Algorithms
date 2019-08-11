6507b7b4d6ee87e11cfdffa4977713ae729f33f5
Srihari Shekhar <52935613+sriharishekhar@users.noreply.github.com>
2019-08-11 12:43:06 -0700

    Update sort_selection.cpp

--- sort_selection.cpp
+++ sort_selection.cpp
@@ -1,8 +1,8 @@
 // Selection sort
 // Algo:
-// We assume the element at the first index of the array to be the smallest value element
+// We assume the element at the first index of the vector to be the smallest value element
 // We put the address of this element in a variable minimum_index
-// we traverse the array and compare value of every element with the value of element at minimum_index
+// we traverse the vector and compare value of every element with the value of element at minimum_index
 // if the value is lower than that at minimum_index, we will put the address of that element in minimum_index
 // this way, at the end of the first traversal, we will have the address of smallest element at minimum_index
 // we will then swap the value at minimum_index with the element at position(number of traversal,in this case 1 i.e element at index 0)
@@ -13,38 +13,41 @@
 // Use : As selection sort does sorting in-place, therefore it doesn't require any extra space.
 
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-void swap (int *a, int *b) {
-  int temp;
-  temp = *a;
+void swap(int* a, int* b) {
+  int temp = *a;
   *a = *b;
   *b = temp;
 }
 
-void print (int a[], int n) {
-  for (int i = 0; i < n; i++) {
-    cout << a[i] << " ";
+void selectionsort(vector<int>& v) {
+  for (int i = 0; i < v.size(); i++) {
+  	int min_index = i;
+  	for (int j = i; j < v.size(); j++) {
+  	  if (v[j] < v[min_index]) {
+  	  	min_index = j;
+  	  }
+  	}
+  	swap (v[i],v[min_index]);
   }
 }
 
-void selection_sort(int a[], int n) {
-  int min_index;
-  for (int i = 0; i < n; i++) {
-    min_index = i;
-    for (int j = i; j < n; j++) {
-      if (a[min_index] > a[j]) 
-        min_index = j;
-    }
-    swap (&a[min_index], &a[i]);
+void print(const vector<int>& v) {
+  for (int i = 0; i < v.size(); i++) {
+  	cout << v[i] << " ";
   }
-  print(a, n);
   cout << endl;
 }
 
 int main() {
-  int a[] = {8,9,12,5,0,4};
-  int n = sizeof(a)/sizeof(a[0]);
-  selection_sort(a,n);
+  vector<int> v {13,4,1,42,3,6};
+  cout << "Unsorted Vector: ";
+  print(v);
+  selectionsort(v);
+  cout << "Sorted Vector: ";
+  print(v);
   return 0;
-} 
+}
