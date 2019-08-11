f73eae96f70e12e5ead9745b557695daf688f35f
Srihari Shekhar <52935613+sriharishekhar@users.noreply.github.com>
2019-08-11 10:36:55 -0700

    Update sort_selection.cpp

--- sort_selection.cpp
+++ sort_selection.cpp
@@ -1,3 +1,17 @@
+// Selection sort
+// Algo:
+// We assume the element at the first index of the array to be the smallest value element
+// We put the address of this element in a variable minimum_index
+// we traverse the array and compare value of every element with the value of element at minimum_index
+// if the value is lower than that at minimum_index, we will put the address of that element in minimum_index
+// this way, at the end of the first traversal, we will have the address of smallest element at minimum_index
+// we will then swap the value at minimum_index with the element at position(number of traversal,in this case 1 i.e element at index 0)
+// Therefore, we will have the first element sorted
+// do this for the remaining n - 1 elements from position 1 - n 
+
+// Time complexity for selection sort is O(n*n)
+// Use : As selection sort does sorting in-place, therefore it doesn't require any extra space.
+
 #include <iostream>
 using namespace std;
 
