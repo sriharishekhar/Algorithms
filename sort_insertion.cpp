a3f05b02b36bc940f717a7389062dde13658d637
Srihari Shekhar <52935613+sriharishekhar@users.noreply.github.com>
2019-08-11 10:44:38 -0700

    Update sort_insertion.cpp

--- sort_insertion.cpp
+++ sort_insertion.cpp
@@ -1,3 +1,15 @@
+// Insertion sort
+// Algo: At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list, and inserts it there. 
+// Repeat until no input elements remain. 
+// At each array-position, check the value there against the largest value in the sorted list 
+// If larger, leave the element in place and move to the next
+// If smaller, find the correct position within the sorted list, shift all the larger values up to make a space, and insert into that correct position.
+
+// Time Complexity : O(n^2)
+
+// Uses : 1. Insertion sort is used when number of elements is small. 
+//        2. It can also be useful when input array is almost sorted, only few elements are misplaced in complete big array.
+
 #include <iostream>
 using namespace std;
 
