ee83a0390945e6b2936762e9a4fb2cb23d3e43c2
Srihari Shekhar <52935613+sriharishekhar@users.noreply.github.com>
2019-08-11 18:35:01 -0700

    Update sort_merge.cpp

--- sort_merge.cpp
+++ sort_merge.cpp
@@ -11,68 +11,68 @@
 //        2. Inversion Count Problem
 //        3. External Sorting
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void merge(int a[], int l, int m, int r) {
+void merge(vector<int>& v, int l, int m, int r) {
   int n1 = m - l + 1;
   int n2 = r - m;
   int L[n1], R[n2];
   int i, j, k;
   for (i = 0; i < n1; i++) {
-    L[i] = a[l + i];
+    L[i] = v[l + i];
   }
   for (j = 0; j < n2; j++) {
-    R[j] = a[m + 1 + j];
+    R[j] = v[m + 1 + j];
   }
   i = 0;
   j = 0;
   k = l;
   while (i < n1 && j < n2) {
     if (L[i] <= R[j]) {
-      a[k++] = L[i];
+      v[k++] = L[i];
       i++;
     }
     else {
-      a[k++] = R[j];
+      v[k++] = R[j];
       j++;
     }
   }
   while (i < n1) {
-    a[k] = L[i];
+    v[k] = L[i];
     i++;
     k++;
   }
   while (j < n2) {
-    a[k] = R[j];
+    v[k] = R[j];
     j++;
     k++;
   }
 }
 
-void merge_sort(int a[], int l, int r) {
+void merge_sort(vector<int>& v, int l, int r) {
   if (l < r) {
     int m = l + (r-l) / 2;
-    merge_sort(a,l,m);
-    merge_sort(a,m+1,r);
-    merge(a,l,m,r);
+    merge_sort(v,l,m);
+    merge_sort(v,m+1,r);
+    merge(v,l,m,r);
   }  
 }
 
-void print(int a[], int n) {
-  for (int i = 0; i < n; i++) {
-  	cout << a[i] << " ";
+void print(const vector<int>& v) {
+  for (int i = 0; i < v.size(); i++) {
+    cout << v[i] << " ";
   }
   cout << endl;
 }
 
 int main() {
-  int a[] = {13, 4, 1, 42, 3, 6};
-  int n = sizeof(a)/sizeof(a[0]);
+  vector<int> v {13, 4, 1, 42, 3, 6};
   cout << "Unsorted array: ";
-  print(a,n);
-  merge_sort(a,0,n-1);
+  print(v);
+  merge_sort(v,0,v.size()-1);
   cout << "Sorted array: ";
-  print(a,n);
+  print(v);
   return 0;
 }
