9653334b571826a058e5dd43f939159e199b6d54
Srihari Shekhar <52935613+sriharishekhar@users.noreply.github.com>
2019-08-11 18:35:23 -0700

    Update sort_quick.cpp

--- sort_quick.cpp
+++ sort_quick.cpp
@@ -7,53 +7,51 @@
 // Time Complexoty : O(nlogn)
 // The sorting takes in-place and hence no extra memory is allocated
 
-
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void swap(int *a, int *b) {
-  int temp;
-  temp = *a;
+  int temp = *a;
   *a = *b;
   *b = temp;
 }
 
-void print(int a[], int n) {
-  for (int i = 0; i < n; i++) {
-    cout << a[i] << " ";
+void print(const vector<int>& v) {
+  for (int i = 0; i < v.size(); i++) {
+    cout << v[i] << " ";
   }
   cout << endl;
 }
 
-int partition(int a[], int first, int last) {
-  int pivot = a[last];
+int partition(vector<int>& v, int first, int last) {
+  int pivot = v[last];
   int i = first - 1;
   for (int j = first; j <= last - 1; j++) {
-    if (a[j] <= pivot) {
+    if (v[j] <= pivot) {
       i++;
-      swap(&a[i], &a[j]);
+      swap(&v[i], &v[j]);
     }
   }
-  swap (&a[i + 1], &a[last]);
+  swap (&v[i+1], &v[last]);
   return (i + 1);
 }
 
-void quick_sort(int a[], int first, int last) {
+void quick_sort(vector<int>& v, int first, int last) {
   if (first < last) {
-    int partition_value = partition(a,first,last);
-    quick_sort(a,first, partition_value-1);
-    quick_sort(a, partition_value+1, last);
+    int partition_value = partition(v,first,last);
+    quick_sort(v,first, partition_value-1);
+    quick_sort(v, partition_value+1, last);
   }
 }
 
 int main() {
-  int a[] = {13, 4, 1, 42, 3, 6};
-  int n = sizeof(a)/sizeof(a[0]);
-  cout << "Unsorted array: ";
-  print(a,n);
-  quick_sort(a, 0, n-1);
-  cout << "Sorted array: ";
-  print(a,n);
+  vector<int> v = {13, 4, 1, 42, 3, 6};
+  cout << "Unsorted Vector: ";
+  print(v);
+  quick_sort(v, 0, v.size()-1);
+  cout << "Sorted Vector: ";
+  print(v);
   return 0;
 }
